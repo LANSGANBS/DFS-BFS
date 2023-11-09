d7b63045e7ba97b7003624e976d653e2e652ce4a
LANSGANBS <2140967451@qq.com>
2023-11-09 18:20:54 +0800

    main

--- Oil Deposits 油田问题（基础DFS深搜）.cpp
+++ Oil Deposits 油田问题（基础DFS深搜）.cpp
@@ -1,12 +1,112 @@
+// C
+#ifndef _GLIBCXX_NO_ASSERT
+#include <cassert>
+#endif
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
 #include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+
+#if __cplusplus >= 201103L
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#endif
+
+// C++
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
 #include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+#if __cplusplus >= 201103L
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#endif
+#define endl '\n'
+#define buff ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+/*
+#pragma GCC optimize ("Ofast")
+#pragma GCC optimize ("unroll-loops")
+#pragma GCC optimize(3)
+*/
 using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vll;
+typedef vector<pair<ll, ll>> vpll;
+const ll MAX_INT = 0x3f3f3f3f;
+const ll MAX_LL = 0x3f3f3f3f3f3f3f3f;
+const ll CF = 2e5 + 9;
+const ll mod = 1e9 + 7;
 char abc[120][120];
 int m, n;
 int i, j, k;
 int ans;
 int a[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
 int b[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+
 void dfs(int x, int y)
 {
     int k;
@@ -20,15 +120,14 @@ void dfs(int x, int y)
     }
     return;
 }
-int main()
+
+void solve()
 {
-    int m, n;
-    while (scanf("%d%d", &m, &n) != EOF && (m || n))
+    while (cin >> m >> n && (m || n))
     {
-        int i, j;
-        int ans = 0;
+        ans = 0;
         for (i = 0; i < m; i++)
-            scanf("%s", abc[i]);
+            cin >> abc[i];
         for (i = 0; i < m; i++)
         {
             for (j = 0; j < n; j++)
@@ -40,7 +139,18 @@ int main()
                 }
             }
         }
-        printf("%d\n", ans);
+        cout << ans << endl;
+    }
+}
+
+int main()
+{
+    buff;
+    int t = 1;
+    //	cin >> t;
+    while (t--)
+    {
+        solve();
     }
     return 0;
 }
